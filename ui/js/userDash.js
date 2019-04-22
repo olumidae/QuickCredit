5f714f725c41bcde9a0fa4298e1629d5a867ba08
olumidae <oomitiran@gmail.com>
2019-04-22 23:43:36 +0100

    feature(user-dashboard):create dashboad for user to apply for loan and view loan history [Finishes #165382114]

---
+++ ui/js/userDash.js
@@ -0,0 +1,9 @@
+const showDiv =()=> {
+    document.getElementById("survey-form").style.display = "block";
+    document.getElementById("table").style.display = "none";
+}
+
+const showLoanHistory =()=> {
+    document.getElementById("table").style.display = "block";
+    document.getElementById("survey-form").style.display = "none";
+}
