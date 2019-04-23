ecac5952c841a4acc4fb78c1944feceef6df167f
olumidae <oomitiran@gmail.com>
2019-04-23 16:22:51 +0100

    feature(view all loans): add page for admin to view all loans [Fininshes #165393637]

--- ui/js/adminDash.js
+++ ui/js/adminDash.js
@@ -1,7 +1,14 @@
 const clientTable =()=> {
-    document.getElementById("clients").style.display = "block"
+    document.getElementById("clients").style.display = "block";
+    document.getElementById("allLoans").style.display = "none";
 }
 
 const verifyClient =()=> {
     alert("Client Verified"); 
+}
+
+// View All Loans Table
+const viewAllLoans =()=> {
+    document.getElementById("allLoans").style.display = "block";
+    document.getElementById("clients").style.display = "none";
 }
