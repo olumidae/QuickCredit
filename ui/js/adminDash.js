1aa8ee34ccb2d58e9ef0672c6142954ab823f7bf
olumidae <oomitiran@gmail.com>
2019-04-24 00:33:53 +0100

    feature(view repiad loans):create page to view repaid loans [Finishes #165394321]

--- ui/js/adminDash.js
+++ ui/js/adminDash.js
@@ -2,6 +2,7 @@ const clientTable =()=> {
     document.getElementById("clients").style.display = "block";
     document.getElementById("allLoans").style.display = "none";
     document.getElementById("unpaidLoans").style.display = "none";
+    document.getElementById("repaidLoans").style.display = "none";
 }
 
 const verifyClient =()=> {
@@ -12,10 +13,19 @@ const viewAllLoans =()=> {
     document.getElementById("allLoans").style.display = "block";
     document.getElementById("clients").style.display = "none";
     document.getElementById("unpaidLoans").style.display = "none";
+    document.getElementById("repaidLoans").style.display = "none";
 }
 
 const viewUnpaidLoans =()=> {
     document.getElementById("unpaidLoans").style.display = "block";
     document.getElementById("clients").style.display = "none";
     document.getElementById("allLoans").style.display = "none";
+    document.getElementById("repaidLoans").style.display = "none";
+}
+
+const viewRepaidLoans =()=> {
+    document.getElementById("repaidLoans").style.display = "block";
+    document.getElementById("clients").style.display = "none";
+    document.getElementById("allLoans").style.display = "none";
+    document.getElementById("unpaidLoans").style.display = "none";
 }
