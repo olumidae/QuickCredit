83afb607db6e715752e3a6b1072a57e50241ec9f
olumidae <oomitiran@gmail.com>
2019-04-26 19:26:59 +0100

    feature(post loan repayment): add post loan repayment page [Finishes #165398013]

--- ui/js/userDash.js
+++ ui/js/userDash.js
@@ -1,9 +1,9 @@
 const showDiv =()=> {
-    document.getElementById("survey-form").style.display = "block";
+    document.getElementById("loan-form").style.display = "block";
     document.getElementById("table").style.display = "none";
 }
 
 const showLoanHistory =()=> {
     document.getElementById("table").style.display = "block";
-    document.getElementById("survey-form").style.display = "none";
+    document.getElementById("loan-form").style.display = "none";
 }
