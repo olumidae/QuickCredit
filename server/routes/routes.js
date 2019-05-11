3fc3ca6875108a3add518dc0be8f7f904c5016e2
olumidae <oomitiran@gmail.com>
2019-05-11 22:21:32 +0100

    feature(apply-for-loan):add endpoint to apply for loan [Finishes #165822510]

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import User from '../controllers/userController';
+import Loan from '../controllers/loanController';
 // create the express router that will have all endpoints
 const router = express.Router();
 
@@ -9,5 +10,11 @@ const router = express.Router();
 router.get('/api/v1/users', User.getAllUsers);
 router.post('/api/v1/auth/signup', User.signUp);
 router.post('/api/v1/auth/signin', User.logIn);
+router.patch('/api/v1/users/:email', User.verifyUser);
+
+// Loan Handler
+router.get('api/v1/loans', Loan.getAllLoans);
+router.post('/api/v1/loans/apply', Loan.applyForLoan);
+
 
 export default router;
--- server/utils/authenticateUser.js
+++ server/utils/authenticateUser.js
@@ -1,6 +1,6 @@
 import jo from 'joi';
 
-exports.UserLoginValidator = (user) => {
+const UserLoginValidator = (user) => {
   const loginformat = jo.object().keys({
     email: jo.string().email().regex(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
     password: jo.string().regex(/^[a-zA-Z0-9]{4,8}$/).min(6).required(),
@@ -8,14 +8,14 @@ exports.UserLoginValidator = (user) => {
   return jo.validate(user, loginformat);
 };
 
-exports.signupValidator = (user) => {
+const signupValidator = (user) => {
   const signupFormat = {
     firstName: jo.string().min(3).trim().required()
       .regex(/^[A-Za-z]+$/),
     lastName: jo.string().min(3).required(),
     email: jo.string().email().regex(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).required(),
     password: jo.string().regex(/^[a-zA-Z0-9]{4,8}$/).min(4).required(),
-    address: jo.string().min(3).required(),
+    address: jo.string().min(3),
     status: jo.string().valid('verified', 'unverified'),
     isAdmin: jo.boolean().valid('true', 'false'),
     isLoggedIn: jo.boolean().valid('true', 'false'),
@@ -23,11 +23,17 @@ exports.signupValidator = (user) => {
   return jo.validate(user, signupFormat);
 };
 
-exports.verifyUserValidator = (user) => {
+const verifyUserValidator = (user) => {
   const updateFormat = {
     status: jo.string().valid('verified', 'unverified').required(),
     verifiedBy: jo.string().required(),
 
   };
   return jo.validate(user, updateFormat);
+};
+
+export default {
+  verifyUserValidator,
+  signupValidator,
+  UserLoginValidator,
 };
