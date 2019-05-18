e15a5ec66536b0245230e93361d598f0f3fc3f6f
olumidae <oomitiran@gmail.com>
2019-05-18 11:18:07 +0100

    ch(heroku):deploy to heroku [Finishes #166083496]

--- server/test/users.js
+++ server/test/users.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-undef */
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import app from '../../app';
+import app from '../app';
 
 const { expect } = chai;
 chai.use(chaiHttp);
